dc8814b04bf7ed5ce633550b5c53435f9aab0953
Nha Phuong <nhaphuong5994@gmail.com>
2022-03-26 11:20:03 +0700

    initial-setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PageHeader from './components/Page-Header';
+// import Avatar from "./Avatar.js"
+import './styles/global.scss';
+
+
+function App() {
+  return (
+    <div>
+        <div className='App'>
+        <PageHeader/>
+        </div>
+        
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Page-Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.scss";
+
+const PageHeader = () => {
+  return (
+    <div className="page-header">
+      <div className="title">character</div>
+      <div className="subtitle">customization</div>
+      <div className="devider"></div>
+    </div>
+  );
+};
+
+export default PageHeader;
